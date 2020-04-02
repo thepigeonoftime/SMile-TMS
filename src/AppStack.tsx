ff81bd9abad30848c9c8d2c49f3192b0a948a6e7
timepigeon <anatol@riseup.net>
2020-04-02 17:14:01 +0200

    Replace testing Appstack with basic functional version

--- src/AppStack.tsx
+++ src/AppStack.tsx
@@ -1,60 +1,106 @@
-import React, { useContext } from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { AppProps } from "./Types";
-import { AntDesign, Ionicons, EvilIcons } from "@expo/vector-icons";
-import { Home } from "./Home";
-import { Maps } from "./Maps";
-import { Route } from "./Route";
-import { Settings } from "./Settings";
-import { Signature } from "./Signature";
-import { QRTest } from "./QRTest";
-import { Text } from "react-native";
-import { RegisterContext } from "./RegisterProvider";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { Center } from "./Center";
-
+import {EvilIcons, MaterialCommunityIcons} from "@expo/vector-icons";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {createStackNavigator} from "@react-navigation/stack";
+import React, {useContext} from "react";
+import {StyleSheet, View} from "react-native";
+import {Home} from "./Home";
+import {RegisterContext} from "./RegisterProvider";
+import {Route} from "./Route";
+import {TestStack} from "./TestStack";
+import {AppProps} from "./Types";
 
 const Tabs = createBottomTabNavigator<AppProps>();
 
-export const AppStack: React.FC<{}> = ({ }) => {
-  const { registered, unregister } = useContext(RegisterContext);
-  return (
-    // <Center>
-    //   <Text>registered: { registered }</Text>
-    //   <TouchableOpacity onPress={() => { unregister();   }}><Text>Unregister</Text></TouchableOpacity>
-    // </Center>
-    <Tabs.Navigator
-    screenOptions={({ route }) => ({
-      tabBarIcon: ({ focused, color, size }) => {
-
-        if (route.name === "Home") {
-          return <AntDesign name={"home"} size={size} color={color} />;
-        } else if (route.name === "Route") {
-            return <EvilIcons name={"search"} size={size} color={color} />;
-        } else if (route.name === "Maps") {
-            return <Ionicons name={"md-map"} size={size} color={color} />;
-        } else if (route.name === "Settings") {
-            return <EvilIcons name={"gear"} size={size} color={color} />;
-        } else if (route.name === "QRTest") {
-          return <AntDesign name={"scan1"} size={size} color={color} />;
-        } else if (route.name === "Signature") {
-          return <AntDesign name={"edit"} size={size} color={color} />;
-        }
-        // default fallback
-        return <AntDesign name={"home"} size={size} color={color} />;
-      }
-    })}
-    tabBarOptions={{
-      activeTintColor: "tomato",
-      inactiveTintColor: "gray"
-    }}
-    >
-    <Tabs.Screen name="Home" component={Home} />
-    <Tabs.Screen name="Route" component={Route} />
-    <Tabs.Screen options={{ title: "Karte" }} name="Maps" component={Maps} />
-    <Tabs.Screen options={{ title: "QR Codes" }} name="QRTest" component={QRTest} />
-    <Tabs.Screen name="Signature" component={Signature} />
-    <Tabs.Screen options={{title: "Einstellungen"}} name="Settings" component={Settings} />
-    </Tabs.Navigator>
-  );
+export const AppTabs: React.FC<{}> = ({}) => {
+    const {registered, unregister} = useContext(RegisterContext);
+    return (
+        <View style={styles.tabContainer}>
+            <Tabs.Navigator
+                screenOptions={({route}) => ({
+                    header: null,
+                    headerMode: null,
+                    tabBarIcon: ({focused, color, size}) => {
+                        if (route.name === "TourStart") {
+                            return <EvilIcons name={"location"} size={size} color={color} />;
+                        } else if (route.name === "TourLogbuch") {
+                            return (
+                                <MaterialCommunityIcons
+                                    name={"clipboard-check-outline"}
+                                    size={size}
+                                    color={color}
+                                />
+                            );
+                        } else if (route.name === "Settings") {
+                            return <EvilIcons name={"gear"} size={size} color={color} />;
+                        }
+                    }
+                })}
+                tabBarOptions={{
+                    activeTintColor: "#41A9F5",
+                    inactiveTintColor: "gray",
+                    style: styles.tabBar,
+                    labelStyle: styles.tabLabel
+                }}
+            >
+                <Tabs.Screen
+                    name="TourStart"
+                    options={{tabBarVisible: true, title: "Tour anfordern"}}
+                    component={Home}
+                />
+                <Tabs.Screen
+                    name="TourLogbuch"
+                    options={{tabBarVisible: true, title: "Tourenlogbuch"}}
+                    component={Route}
+                />
+                <Tabs.Screen
+                    options={{title: "Einstellungen"}}
+                    name="Settings"
+                    component={TestStack}
+                />
+            </Tabs.Navigator>
+        </View>
+    );
 };
+
+const Stack = createStackNavigator();
+
+export const AppStack = () => {
+    return (
+        <Stack.Navigator mode="modal" headerMode="none">
+            <Stack.Screen name="Home" component={AppTabs} options={{}} />
+        </Stack.Navigator>
+    );
+};
+
+const styles = StyleSheet.create({
+    tabContainer: {
+        height: "100%",
+        backgroundColor: "transparent"
+    },
+    tabBar: {
+        bottom: "3%",
+        left: "5%",
+        width: "90%",
+        height: 70,
+        padding: 0,
+        margin: 0,
+        fontSize: 30,
+        borderRadius: 30,
+        borderTopColor: "transparent",
+        backgroundColor: "#FFF",
+        paddingTop: 15,
+        paddingBottom: 15,
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowRadius: 4.65,
+        elevation: 5,
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        position: "absolute"
+    },
+    tabLabel: {
+        fontSize: 13
+    }
+});
--- src/Types.ts
+++ src/Types.ts
@@ -1,57 +1,52 @@
-import { StackNavigationProp } from "@react-navigation/stack";
-import { RouteProp } from "@react-navigation/native";
+import {RouteProp} from "@react-navigation/native";
+import {StackNavigationProp} from "@react-navigation/stack";
 
 export type AuthProps = {
-  Login: undefined;
-  Register: undefined;
+    Login: undefined;
+    Register: undefined;
 };
 
 export type AuthNavProps<T extends keyof AuthProps> = {
-  navigation: StackNavigationProp<AuthProps, T>;
-  route: RouteProp<AuthProps, T>;
+    navigation: StackNavigationProp<AuthProps, T>;
+    route: RouteProp<AuthProps, T>;
 };
 
 export type AppProps = {
-  Home: undefined;
-  Maps: undefined;
-  Route: undefined;
-  CodeScanner: undefined;
-  Settings: undefined;
-  Signature: undefined;
-  CodeGenerator: undefined;
-  QRTest: undefined;
+    TourStart: undefined;
+    TourLogbuch: undefined;
+    Settings: undefined;
 };
 
 export type HomeProps = {
-  Home: undefined;
+    Home: undefined;
 };
 
 export type MapsProps = {
-  Maps: undefined;
+    Maps: undefined;
 };
 
 export type RouteProps = {
-  Route: undefined;
+    Route: undefined;
 };
 
 export type ScannerProps = {
-  CodeScanner: undefined;
+    CodeScanner: undefined;
 };
 
 export type GeneratorProps = {
-  CodeGenerator: undefined;
+    CodeGenerator: undefined;
 };
 
 export type QRTestProps = {
-  QRTest: undefined;
-  QRScan: undefined;
-  QRGenerator: undefined;
+    QRTest: undefined;
+    QRScan: undefined;
+    QRGenerator: undefined;
 };
 
 export type SignatureProps = {
-  Signature: undefined;
+    Signature: undefined;
 };
 
 export type SettingsProps = {
-  Settings: undefined;
+    Settings: undefined;
 };
