30e933a75776ad7c43299cc406ab3ff839ca03f3
timepigeon <anatol@riseup.net>
2020-04-09 17:27:37 +0200

    Add form validation to RegisterPerson

--- src/Screens/RegisterPerson.tsx
+++ src/Screens/RegisterPerson.tsx
@@ -1,11 +1,60 @@
 import {AntDesign, FontAwesome, MaterialIcons, SimpleLineIcons} from "@expo/vector-icons";
-import {Formik} from "formik";
-import React from "react";
-import {Alert, Keyboard, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import React, {useEffect} from "react";
+import {Controller, useForm} from "react-hook-form";
+import {Alert, ScrollView, StyleSheet, TouchableOpacity, View} from "react-native";
 import {Button, Input} from "react-native-elements";
+import * as Yup from "yup";
 import {Header} from "../Header";
 
 export const RegisterPerson = ({navigation}) => {
+    const registerSchema = Yup.object().shape({
+        vorname: Yup.string().min(2, "Zu kurz").max(20, "Zu lang").required("Benötigt"),
+        nachname: Yup.string().min(2, "Zu kurz").max(20, "Zu lang").required("Benötigt"),
+        firma: Yup.string().min(2, "Zu kurz").max(30, "Zu lang").required("Benötigt"),
+        strasse: Yup.string().min(2, "Zu kurz").max(30, "Zu lang").required("Benötigt"),
+        hausnummer: Yup.string()
+            .min(1, "Zu kurz")
+            .max(7, "Zu lang")
+            .matches(/^\d/gi, "Ungültig")
+            .required("Benötigt"),
+        plz: Yup.string()
+            .matches(/^[0-9]{5}$/, "5-stellige Zahl")
+            .required("Benötigt"),
+        ort: Yup.string().min(2, "Zu kurz").max(50, "Zu lang").required("Benötigt"),
+        telefon: Yup.string().matches(
+            /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
+            "Telefonnummer nicht gültig"
+        ),
+    });
+    const {
+        register,
+        setValue,
+        handleSubmit,
+        errors,
+        triggerValidation,
+        control,
+        formState,
+    } = useForm({
+        mode: "onBlur",
+        reValidateMode: "onChange",
+        validationSchema: registerSchema, // Note: will be deprecated in the next major version with validationResolver
+        // validationResolver: undefined,
+        // validationContext: undefined,
+        // validateCriteriaMode: "firstError",
+        submitFocusError: true,
+    });
+    const onSubmit = (data) => Alert.alert("Form Data", JSON.stringify(data));
+
+    useEffect(() => {
+        register({name: "vorname"});
+        register({name: "nachname"});
+        register({name: "firma"});
+        register({name: "strasse"});
+        register({name: "hausnummer"});
+        register({name: "plz"});
+        register({name: "ort"});
+        register({name: "telefon"});
+    }, [register]);
     return (
         <ScrollView>
             <View>
@@ -19,113 +68,152 @@ export const RegisterPerson = ({navigation}) => {
                 <TouchableOpacity style={styles.closeButton} onPress={() => navigation.goBack()}>
                     <AntDesign name={"close"} size={20} color="#f89e3b" />
                 </TouchableOpacity>
-                <Formik
-                    initialValues={{
-                        firstName: "",
-                        lastName: "",
-                        company: "",
-                        streetName: "",
-                        postCode: "",
-                        province: "",
-                        phone: "",
-                    }}
-                    onSubmit={(values) => {
-                        Alert.alert(JSON.stringify(values, null, 2));
-                        Keyboard.dismiss();
-                    }}
-                >
-                    {({handleChange, handleSubmit, values}) => (
-                        <View>
-                            <View>
-                                <View style={styles.inputWrap}>
-                                    <SimpleLineIcons name="user" size={25} color="#ccc" />
-                                    <Input
-                                        containerStyle={styles.input}
-                                        onChangeText={handleChange("firstName")}
-                                        value={values.firstName}
-                                        label="Vorname"
-                                        placeholder="Vorname"
-                                    />
-                                </View>
-                                <View style={styles.inputWrap}>
-                                    <SimpleLineIcons name="user" size={25} color="#FFF" />
-                                    <Input
-                                        containerStyle={styles.input}
-                                        onChangeText={handleChange("lastName")}
-                                        value={values.lastName}
-                                        label="Nachname"
-                                        placeholder="Nachname"
-                                    />
-                                </View>
-                                <View style={styles.inputWrap}>
-                                    <MaterialIcons name="store" size={30} color="#ccc" />
-                                    <Input
-                                        containerStyle={styles.input}
-                                        style={styles.input}
-                                        onChangeText={handleChange("company")}
-                                        value={values.company}
-                                        label="Firma"
-                                        placeholder="Firma"
-                                    />
-                                </View>
-                                <View style={styles.inputWrap}>
-                                    <FontAwesome name="map-signs" size={25} color="#ccc" />
-                                    <Input
-                                        containerStyle={styles.input}
-                                        style={styles.input}
-                                        onChangeText={handleChange("streetName")}
-                                        value={values.streetName}
-                                        label="Straße"
-                                        placeholder="Straße"
-                                    />
-                                </View>
-                                <View style={[styles.inputWrap, styles.halfInputContainer]}>
-                                    <SimpleLineIcons name="user" size={25} color="#fff" />
-                                    <Input
-                                        containerStyle={styles.halfInput}
-                                        style={styles.input}
-                                        onChangeText={handleChange("postCode")}
-                                        value={values.postCode}
-                                        label="Postleitzahl"
-                                        placeholder="Postleitzahl"
-                                    />
-                                    <Input
-                                        containerStyle={styles.halfInput}
-                                        style={styles.input}
-                                        onChangeText={handleChange("province")}
-                                        value={values.province}
-                                        label="Ort"
-                                        placeholder="Ort"
-                                    />
-                                </View>
-                                <View style={styles.inputWrap}>
-                                    <MaterialIcons name="phone-android" size={25} color="#ccc" />
-                                    <Input
-                                        containerStyle={styles.input}
-                                        style={styles.input}
-                                        onChangeText={handleChange("phone")}
-                                        value={values.phone}
-                                        label="Mobiltelefon"
-                                        placeholder="Mobiltelefon"
-                                    />
-                                </View>
-                            </View>
-                            <View style={styles.saveButtonContainer}>
-                                <Button
-                                    buttonStyle={styles.saveButton}
-                                    titleStyle={styles.saveButtonTitle}
-                                    disabledStyle={styles.saveButtonDisabled}
-                                    disabled={true}
-                                    title="Speichern"
-                                    onPress={() => {
-                                        handleSubmit;
-                                        navigation.goBack();
-                                    }}
-                                />
-                            </View>
+
+                <View>
+                    <View>
+                        <View style={styles.inputWrap}>
+                            <SimpleLineIcons
+                                name="user"
+                                size={25}
+                                color="#ccc"
+                                style={{marginTop: 10}}
+                            />
+                            <Controller
+                                as={<Input />}
+                                name={"vorname"}
+                                control={control}
+                                label="Vorname"
+                                placeholder="Vornamen eingeben"
+                                onChangeText={(text) => {
+                                    setValue("vorname", text);
+                                }}
+                                errorMessage={errors.vorname ? errors.vorname.message : " "}
+                                containerStyle={styles.inputContainer}
+                                inputContainerStyle={errors.vorname && styles.inputError}
+                                errorStyle={styles.error}
+                            />
                         </View>
-                    )}
-                </Formik>
+                        <View style={styles.inputWrap}>
+                            <SimpleLineIcons name="user" size={25} color="#FFF" />
+                            <Controller
+                                as={<Input />}
+                                name={"nachname"}
+                                control={control}
+                                label="Nachname"
+                                placeholder="Nachnamen eingeben"
+                                onChangeText={(text) => setValue("nachname", text)}
+                                errorMessage={errors.nachname ? errors.nachname.message : " "}
+                                containerStyle={styles.inputContainer}
+                                inputContainerStyle={errors.nachname && styles.inputError}
+                                errorStyle={styles.error}
+                            />
+                        </View>
+                        <View style={styles.inputWrap}>
+                            <MaterialIcons name="store" size={30} color="#ccc" />
+                            <Controller
+                                as={<Input />}
+                                name={"firma"}
+                                control={control}
+                                label="Firma"
+                                placeholder="Firmenname eingeben"
+                                onChangeText={(text) => setValue("firma", text)}
+                                errorMessage={errors.firma ? errors.firma.message : " "}
+                                containerStyle={styles.inputContainer}
+                                inputContainerStyle={errors.firma && styles.inputError}
+                                errorStyle={styles.error}
+                            />
+                        </View>
+                        <View style={styles.inputWrap}>
+                            <FontAwesome name="map-signs" size={25} color="#ccc" />
+                            <Controller
+                                as={<Input />}
+                                name={"strasse"}
+                                control={control}
+                                label="Straße / Hausnummer"
+                                placeholder="Straße eingeben"
+                                onChangeText={(text) => setValue("strasse", text)}
+                                errorMessage={errors.strasse ? errors.strasse.message : " "}
+                                containerStyle={styles.strasseInputContainer}
+                                inputContainerStyle={errors.strasse && styles.inputError}
+                                errorStyle={styles.error}
+                            />
+                            <Controller
+                                as={<Input />}
+                                name={"hausnummer"}
+                                control={control}
+                                label=" "
+                                placeholder="Nr."
+                                onChangeText={(text) => {
+                                    setValue("hausnummer", text);
+                                    errors.hausnummer && triggerValidation("hausnummer");
+                                }}
+                                errorMessage={errors.hausnummer ? errors.hausnummer.message : " "}
+                                containerStyle={styles.nrInputContainer}
+                                inputContainerStyle={errors.hausnummer && styles.inputError}
+                                errorStyle={styles.error}
+                            />
+                        </View>
+                        <View style={[styles.inputWrap, styles.halfInputWrap]}>
+                            <SimpleLineIcons name="user" size={25} color="#fff" />
+                            <Controller
+                                as={<Input />}
+                                name={"plz"}
+                                control={control}
+                                label="Postleitzahl"
+                                placeholder="Postleitzahl"
+                                onChangeText={(text) => {
+                                    setValue("plz", text);
+                                    errors.plz && triggerValidation("plz");
+                                }}
+                                errorMessage={errors.plz ? errors.plz.message : " "}
+                                inputStyle={styles.input}
+                                containerStyle={styles.halfInputContainer}
+                                inputContainerStyle={errors.plz && styles.inputError}
+                                errorStyle={styles.error}
+                            />
+                            <Controller
+                                as={<Input />}
+                                name={"ort"}
+                                control={control}
+                                label="Ort"
+                                placeholder="Ort"
+                                onChangeText={(text) => setValue("ort", text)}
+                                errorMessage={errors.ort ? errors.ort.message : " "}
+                                containerStyle={styles.halfInputContainer}
+                                inputContainerStyle={errors.ort && styles.inputError}
+                                errorStyle={styles.error}
+                            />
+                        </View>
+                        <View style={styles.inputWrap}>
+                            <MaterialIcons name="phone-android" size={25} color="#ccc" />
+                            <Controller
+                                as={<Input />}
+                                name={"telefon"}
+                                control={control}
+                                label="Mobiltelefon"
+                                placeholder="Mobiltelefon"
+                                onChangeText={(text) => {
+                                    setValue("telefon", text);
+                                    errors.telefon && triggerValidation("telefon");
+                                }}
+                                errorMessage={errors.telefon ? errors.telefon.message : " "}
+                                containerStyle={styles.inputContainer}
+                                inputContainerStyle={errors.telefon && styles.inputError}
+                                errorStyle={styles.error}
+                            />
+                        </View>
+                    </View>
+                    <View style={styles.saveButtonContainer}>
+                        <Button
+                            buttonStyle={styles.saveButton}
+                            titleStyle={styles.saveButtonTitle}
+                            disabledStyle={styles.saveButtonDisabled}
+                            disabled={!formState.isValid}
+                            title="Speichern"
+                            onPress={handleSubmit(onSubmit)}
+                        />
+                    </View>
+                </View>
             </View>
         </ScrollView>
     );
@@ -133,31 +221,12 @@ export const RegisterPerson = ({navigation}) => {
 
 export const styles = StyleSheet.create({
     formContainer: {
-        paddingTop: "4%",
+        paddingTop: "7%",
         padding: 20,
         borderRadius: 25,
         backgroundColor: "#FFF",
         height: "100%",
-        paddingBottom: "30%",
-    },
-    input: {
-        backgroundColor: "#FFF",
-        width: "90%",
-        paddingRight: "5%",
-    },
-    halfInputContainer: {
-        paddingLeft: "0.7%",
-    },
-    halfInput: {
-        backgroundColor: "#FFF",
-        width: "45%",
-        paddingRight: "5%",
-    },
-    inputWrap: {
-        flexDirection: "row",
-        justifyContent: "space-around",
-        alignItems: "flex-end",
-        paddingVertical: 10,
+        paddingBottom: "10%",
     },
     closeButton: {
         borderWidth: 0,
@@ -179,6 +248,41 @@ export const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5,
+    },
+    inputWrap: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+        paddingBottom: 5,
+    },
+    input: {
+        color: "#555",
+    },
+    inputContainer: {
+        backgroundColor: "#FFF",
+        width: "90%",
+        paddingRight: "5%",
+    },
+    strasseInputContainer: {
+        width: "65%",
+    },
+    nrInputContainer: {
+        width: "25%",
+    },
+    halfInputWrap: {
+        paddingLeft: "0.7%",
+    },
+    halfInputContainer: {
+        backgroundColor: "#FFF",
+        width: "45%",
+        paddingRight: "5%",
+    },
+    inputError: {
+        borderColor: "#ff8787",
+    },
+    error: {
+        color: "#ff8787",
+        marginLeft: -1,
     },
     saveButtonContainer: {
         alignItems: "center",
