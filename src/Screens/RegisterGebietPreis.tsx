9a6f864fc6540dea81a5b54e92c890b2ae606989
timepigeon <anatol@riseup.net>
2020-04-15 12:06:57 +0200

    Add form validation to RegisterGebietPreis

--- src/Screens/RegisterGebietPreis.tsx
+++ src/Screens/RegisterGebietPreis.tsx
@@ -4,8 +4,58 @@ import React from "react";
 import {Alert, Keyboard, ScrollView, StyleSheet, TouchableOpacity, View} from "react-native";
 import {Button, Input} from "react-native-elements";
 import {Header} from "../Header";
+import {Controller, useForm} from "react-hook-form";
+import * as Yup from "yup";
 
 export const RegisterGebietPreis = ({navigation}) => {
+    const registerSchema = Yup.object().shape({
+        zustellGebietPLZ: Yup.string()
+            .matches(/^[0-9]{5}$/, "5-stellige Zahl")
+            .required("Benötigt"),
+        entfernungTour: Yup.string()
+            .min(1, "Zu kurz")
+            .max(10, "Zu lang")
+            .matches(/^\d+$/, "Nur Zahlen")
+            .required("Benötigt"),
+        vorlaufZeit: Yup.string()
+            .min(1, "Zu kurz")
+            .max(10, "Zu lang")
+            .matches(/^\d+$/, "Nur Zahlen")
+            .required("Benötigt"),
+        grundpreis: Yup.string()
+            .min(1, "Zu kurz")
+            .max(10, "Zu lang")
+            .matches(/^\d+$/, "Nur Zahlen")
+            .required("Benötigt"),
+        preisProKm: Yup.string()
+            .min(1, "Zu kurz")
+            .max(10, "Zu lang")
+            .matches(/^\d+$/, "Nur Zahlen")
+            .required("Benötigt"),
+        preisProStop: Yup.string()
+            .min(1, "Zu kurz")
+            .max(10, "Zu lang")
+            .matches(/^\d+$/, "Nur Zahlen")
+            .required("Benötigt"),
+    });
+    const {
+        register,
+        setValue,
+        handleSubmit,
+        errors,
+        triggerValidation,
+        control,
+        formState,
+    } = useForm({
+        mode: "onBlur",
+        reValidateMode: "onChange",
+        validationSchema: registerSchema,
+        submitFocusError: true,
+    });
+    const onSubmit = (data) => {
+        Alert.alert("Form Data", JSON.stringify(data));
+        navigation.goBack();
+    };
     return (
         <ScrollView>
             <View>
@@ -15,104 +65,135 @@ export const RegisterGebietPreis = ({navigation}) => {
                 <TouchableOpacity style={styles.closeButton} onPress={() => navigation.goBack()}>
                     <AntDesign name={"close"} size={20} color="#f89e3b" />
                 </TouchableOpacity>
-                <Formik
-                    initialValues={{
-                        zustellGebietPLZ: "",
-                        entfernungTour: "",
-                        vorlaufszeitTourStart: "",
-                        grundpreisTour: "",
-                        preisProKm: "",
-                        preisProStop: "",
-                    }}
-                    onSubmit={(values) => {
-                        Alert.alert(JSON.stringify(values, null, 2));
-                        Keyboard.dismiss();
-                    }}
-                >
-                    {({handleChange, handleSubmit, values}) => (
-                        <View>
-                            <View>
-                                <View style={styles.inputWrap}>
-                                    <SimpleLineIcons name="user" size={25} color="#ccc" />
-                                    <Input
-                                        containerStyle={styles.input}
-                                        onChangeText={handleChange("zustellGebietPLZ")}
-                                        value={values.zustellGebietPLZ}
-                                        label="Postleitzahl Zustellgebiet"
-                                        placeholder="PLZ eingeben"
-                                    />
-                                </View>
-                                <View style={styles.inputWrap}>
-                                    <SimpleLineIcons name="user" size={25} color="#FFF" />
-                                    <Input
-                                        containerStyle={styles.input}
-                                        onChangeText={handleChange("entfernungTour")}
-                                        value={values.entfernungTour}
-                                        label="Entfernung je Tour"
-                                        placeholder="Entfernung in km eingeben"
-                                    />
-                                </View>
-                                <View style={styles.inputWrap}>
-                                    <MaterialIcons name="store" size={30} color="#ccc" />
-                                    <Input
-                                        containerStyle={styles.input}
-                                        style={styles.input}
-                                        onChangeText={handleChange("vorlaufszeitTourStart")}
-                                        value={values.vorlaufszeitTourStart}
-                                        label="Vorlaufzeit bis Tour-Start"
-                                        placeholder="Vorlaufzeit in Minuten angeben"
-                                    />
-                                </View>
-                                <View style={styles.inputWrap}>
-                                    <FontAwesome name="map-signs" size={25} color="#ccc" />
-                                    <Input
-                                        containerStyle={styles.input}
-                                        style={styles.input}
-                                        onChangeText={handleChange("grundpreisTour")}
-                                        value={values.grundpreisTour}
-                                        label="Grundpreis pro Tour"
-                                        placeholder="Grundpreis in EUR eingeben"
-                                    />
-                                </View>
-                                <View style={styles.inputWrap}>
-                                    <FontAwesome name="map-signs" size={25} color="#ccc" />
-                                    <Input
-                                        containerStyle={styles.input}
-                                        style={styles.input}
-                                        onChangeText={handleChange("preisProKm")}
-                                        value={values.preisProKm}
-                                        label="Preis pro km"
-                                        placeholder="Kilometerpreis in EUR eingeben"
-                                    />
-                                </View>
-                                <View style={styles.inputWrap}>
-                                    <MaterialIcons name="phone-android" size={25} color="#ccc" />
-                                    <Input
-                                        containerStyle={styles.input}
-                                        style={styles.input}
-                                        onChangeText={handleChange("preisProStop")}
-                                        value={values.preisProStop}
-                                        label="Preis pro Stop"
-                                        placeholder="Preis pro Stop in EUR eingeben"
-                                    />
-                                </View>
-                            </View>
-                            <View style={styles.saveButtonContainer}>
-                                <Button
-                                    buttonStyle={styles.saveButton}
-                                    titleStyle={styles.saveButtonTitle}
-                                    disabledStyle={styles.saveButtonDisabled}
-                                    disabled={false}
-                                    title="Speichern"
-                                    onPress={() => {
-                                        handleSubmit;
-                                        navigation.goBack();
-                                    }}
-                                />
-                            </View>
+                <View>
+                    <View>
+                        <View style={styles.inputWrap}>
+                            <SimpleLineIcons name="user" size={25} color="#ccc" />
+                            <Controller
+                                as={<Input />}
+                                control={control}
+                                name="zustellGebietPLZ"
+                                onChangeText={(text: number) => {
+                                    setValue("zustellGebietPLZ", text);
+                                    errors.zustellGebietPLZ &&
+                                        triggerValidation("zustellGebietPLZ");
+                                }}
+                                label="Postleitzahl Zustellgebiet"
+                                placeholder="PLZ eingeben"
+                                errorMessage={
+                                    errors.zustellGebietPLZ ? errors.zustellGebietPLZ.message : " "
+                                }
+                                containerStyle={styles.input}
+                                inputContainerStyle={errors.zustellGebietPLZ && styles.inputError}
+                                errorStyle={styles.error}
+                            />
                         </View>
-                    )}
-                </Formik>
+                        <View style={styles.inputWrap}>
+                            <SimpleLineIcons name="user" size={25} color="#FFF" />
+                            <Controller
+                                as={<Input />}
+                                control={control}
+                                name="entfernungTour"
+                                onChangeText={(text: number) => {
+                                    setValue("entfernungTour", text);
+                                    errors.entfernungTour && triggerValidation("entfernungTour");
+                                }}
+                                label="Entfernung je Tour"
+                                placeholder="Entfernung in km eingeben"
+                                errorMessage={
+                                    errors.entfernungTour ? errors.entfernungTour.message : " "
+                                }
+                                containerStyle={styles.input}
+                                inputContainerStyle={errors.entfernungTour && styles.inputError}
+                                errorStyle={styles.error}
+                            />
+                        </View>
+                        <View style={styles.inputWrap}>
+                            <MaterialIcons name="store" size={30} color="#ccc" />
+                            <Controller
+                                as={<Input />}
+                                control={control}
+                                name="vorlaufZeit"
+                                onChangeText={(text: number) => {
+                                    setValue("vorlaufZeit", text);
+                                    errors.vorlaufZeit && triggerValidation("vorlaufZeit");
+                                }}
+                                label="Vorlaufzeit bis Tour-Start"
+                                placeholder="Vorlaufzeit in Minuten angeben"
+                                errorMessage={errors.vorlaufZeit ? errors.vorlaufZeit.message : " "}
+                                containerStyle={styles.input}
+                                inputContainerStyle={errors.vorlaufZeit && styles.inputError}
+                                errorStyle={styles.error}
+                            />
+                        </View>
+                        <View style={styles.inputWrap}>
+                            <FontAwesome name="map-signs" size={25} color="#ccc" />
+                            <Controller
+                                as={<Input />}
+                                control={control}
+                                name="grundpreis"
+                                onChangeText={(text: number) => {
+                                    setValue("grundpreis", text);
+                                    errors.grundpreis && triggerValidation("grundpreis");
+                                }}
+                                label="Grundpreis pro Tour"
+                                placeholder="Grundpreis in EUR eingeben"
+                                errorMessage={errors.grundpreis ? errors.grundpreis.message : " "}
+                                containerStyle={styles.input}
+                                inputContainerStyle={errors.grundpreis && styles.inputError}
+                                errorStyle={styles.error}
+                            />
+                        </View>
+                        <View style={styles.inputWrap}>
+                            <FontAwesome name="map-signs" size={25} color="#ccc" />
+                            <Controller
+                                as={<Input />}
+                                control={control}
+                                name="preisProKm"
+                                onChangeText={(text: number) => {
+                                    setValue("preisProKm", text);
+                                    errors.preisProKm && triggerValidation("preisProKm");
+                                }}
+                                label="Preis pro km"
+                                placeholder="Kilometerpreis in EUR eingeben"
+                                errorMessage={errors.preisProKm ? errors.preisProKm.message : " "}
+                                containerStyle={styles.input}
+                                inputContainerStyle={errors.preisProKm && styles.inputError}
+                                errorStyle={styles.error}
+                            />
+                        </View>
+                        <View style={styles.inputWrap}>
+                            <MaterialIcons name="phone-android" size={25} color="#ccc" />
+                            <Controller
+                                as={<Input />}
+                                control={control}
+                                name="preisProStop"
+                                onChangeText={(text: number) => {
+                                    setValue("preisProStop", text);
+                                    errors.preisProStop && triggerValidation("preisProStop");
+                                }}
+                                label="Preis pro Stop"
+                                placeholder="Preis pro Stop in EUR eingeben"
+                                errorMessage={
+                                    errors.preisProStop ? errors.preisProStop.message : " "
+                                }
+                                containerStyle={styles.input}
+                                inputContainerStyle={errors.preisProStop && styles.inputError}
+                                errorStyle={styles.error}
+                            />
+                        </View>
+                    </View>
+                    <View style={styles.saveButtonContainer}>
+                        <Button
+                            buttonStyle={styles.saveButton}
+                            titleStyle={styles.saveButtonTitle}
+                            disabledStyle={styles.saveButtonDisabled}
+                            disabled={!formState.isValid}
+                            title="Speichern"
+                            onPress={handleSubmit(onSubmit)}
+                        />
+                    </View>
+                </View>
             </View>
         </ScrollView>
     );
@@ -135,7 +216,7 @@ export const styles = StyleSheet.create({
     inputWrap: {
         flexDirection: "row",
         justifyContent: "space-around",
-        alignItems: "flex-end",
+        alignItems: "center",
         paddingVertical: 10,
     },
     closeButton: {
@@ -158,6 +239,13 @@ export const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5,
+    },
+    inputError: {
+        borderColor: "#ff8787",
+    },
+    error: {
+        color: "#ff8787",
+        marginLeft: -1,
     },
     saveButtonContainer: {
         alignItems: "center",
