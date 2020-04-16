cec00399e9c65e7fb1051f1dc7919a175f62dcd3
timepigeon <anatol@riseup.net>
2020-04-16 11:29:27 +0200

    Add basic SQLite OfflineQueue

---
+++ src/OfflineQueue.tsx
@@ -0,0 +1,70 @@
+import React, {useEffect} from "react";
+import * as SQLite from "expo-sqlite";
+// import * as SecureStore from "expo-secure-store";
+
+const db = SQLite.openDatabase("offline.db");
+export const OfflineQueue = () => {
+    useEffect(() => {
+        db.transaction((tx) => {
+            tx.executeSql("DROP TABLE IF EXISTS Tasks", []);
+            tx.executeSql(
+                `CREATE TABLE IF NOT EXISTS Tasks (
+                    id INTEGER NOT NULL PRIMARY KEY,
+                    name VARCHAR(30) NOT NULL,
+                    type VARCHAR(30) NOT NULL,
+                    payload VARCHAR(30)
+                    )`,
+                []
+            );
+        });
+    }, []);
+};
+
+export const addJob = (name, type, payload) => {
+    db.transaction((tx) => {
+        tx.executeSql("INSERT INTO Tasks (name, type, payload) VALUES (?, ?, ?)", [
+            name,
+            type,
+            payload,
+        ]);
+    });
+    console.log("job " + name + " added");
+    getJobs();
+};
+
+export const getJob = (id) => {
+    db.transaction((tx) => {
+        tx.executeSql(`
+          SELECT * FROM Tasks WHERE id=${id};
+        `)[0] || undefined;
+    });
+};
+
+export const getJobs = () => {
+    let result;
+    db.transaction((tx) => {
+        tx.executeSql("SELECT * FROM Tasks", [], (_, {rows}) => {
+            result = JSON.stringify(rows);
+            console.log(result);
+        });
+    });
+    return result;
+};
+
+export const completeJob = (name) => {
+    db.transaction((tx) => {
+        tx.executeSql("DELETE FROM Tasks WHERE name=?", [name]);
+    });
+    console.log("job " + name + " deleted");
+    getJobs();
+};
+
+export const runJobs = () => {
+    const cron = setInterval(() => {
+        getJobs().forEach((job) => {
+            switch (job.type) {
+                case "getTours": // run getTours
+            }
+        });
+    }, 5000);
+};
