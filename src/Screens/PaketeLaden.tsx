ad5105d0be4134a336737aac3fc031cf6a029956
timepigeon <anatol@riseup.net>
2020-04-17 10:41:31 +0200

    Add TourRoutes for TourStart

--- src/Screens/PaketeLaden.tsx
+++ src/Screens/PaketeLaden.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import {QRCode} from "react-native-custom-qr-codes-expo";
 import {Center} from "../Center";
 import {GeneratorProps} from "../Types";
+import {TouchableOpacity, Text} from "react-native";
 
 export const PaketeLaden = ({navigation}) => {
     let content = "smile QR Code test";
     return (
         <Center>
             <QRCode codeStyle="square" content={content} size={300} />
+            <TouchableOpacity onPress={navigation.goBack}>
+                <Text style={{marginTop: 50, color: "blue"}}>Zurück</Text>
+            </TouchableOpacity>
         </Center>
     );
 };
--- src/TourStack.tsx
+++ src/TourStack.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import {TourStart} from "./Screens/TourStart";
 import {TourSuche} from "./Screens/TourSuche";
 import {TourStackProps} from "./Types";
+import {Maps} from "./Screens/Maps";
+import {PaketeLaden} from "./Screens/PaketeLaden";
 
 const Stack = createStackNavigator<TourStackProps>();
 
@@ -11,6 +13,8 @@ export const TourStack = () => {
         <Stack.Navigator mode="modal" headerMode="none">
             <Stack.Screen name="TourSuche" component={TourSuche} options={{}} />
             <Stack.Screen name="TourStart" component={TourStart} options={{}} />
+            <Stack.Screen name="Maps" component={Maps} options={{}} />
+            <Stack.Screen name="PaketeLaden" component={PaketeLaden} options={{}} />
         </Stack.Navigator>
     );
 };
