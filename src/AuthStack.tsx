44727f0fcd4f69fbe8c6efefd0a8c69799340702
timepigeon <anatol@riseup.net>
2020-04-17 16:42:03 +0200

    Remove Nav Header from AuthStack

--- src/AuthStack.tsx
+++ src/AuthStack.tsx
@@ -8,24 +8,13 @@ const Stack = createStackNavigator<AuthProps>();
 
 export const AuthStack: React.FC<{}> = ({}) => {
     return (
-        <Stack.Navigator
-            // screenOptions={{
-            //   header: () => null
-            // }}
-            initialRouteName="Login"
-        >
-            <Stack.Screen
-                options={{
-                    headerTitle: "Login",
-                }}
-                name="Login"
-                component={Login}
-            />
+        <Stack.Navigator headerMode="none" initialRouteName="Login">
+            <Stack.Screen name="Login" component={Login} />
             <Stack.Screen
                 options={{
                     headerTitle: "Registrieren",
                 }}
-                name="Register"
+                name="Signup"
                 component={Signup}
             />
         </Stack.Navigator>
