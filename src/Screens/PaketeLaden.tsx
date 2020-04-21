fdb4f85f57f504feb50801e38a6e3ef9a82d38c6
timepigeon <anatol@riseup.net>
2020-04-21 12:01:09 +0200

    Add PaketeLaden modal header

--- src/Screens/PaketeLaden.tsx
+++ src/Screens/PaketeLaden.tsx
@@ -1,17 +1,88 @@
+import {AntDesign} from "@expo/vector-icons";
 import React from "react";
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {QRCode} from "react-native-custom-qr-codes-expo";
-import {Center} from "../Center";
-import {GeneratorProps} from "../Types";
-import {TouchableOpacity, Text} from "react-native";
+import {Header} from "../Header";
 
 export const PaketeLaden = ({navigation}) => {
     let content = "smile QR Code test";
     return (
-        <Center>
-            <QRCode codeStyle="square" content={content} size={300} />
-            <TouchableOpacity onPress={navigation.goBack}>
-                <Text style={{marginTop: 50, color: "blue"}}>Zurück</Text>
-            </TouchableOpacity>
-        </Center>
+        <View style={{flex: 1}}>
+            <Header
+                text="Tourvorbereitung"
+                color="#729628"
+                containerStyle={{
+                    flex: 2,
+                    paddingLeft: "10%",
+                    paddingTop: "10%",
+                    alignItems: "flex-start",
+                    paddingBottom: "3%",
+                    backgroundColor: "#F2F2F2",
+                }}
+            />
+            <View style={styles.container}>
+                <View style={styles.closeButtonContainer}>
+                    <TouchableOpacity
+                        onPress={() => navigation.goBack()}
+                        style={styles.closeButton}
+                    >
+                        <AntDesign name={"close"} size={20} color="#f89e3b" />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.qrContainer}>
+                    <QRCode
+                        codeStyle="square"
+                        content={content}
+                        size={300}
+                        style={{backgroundColor: "#FFF"}}
+                    />
+                </View>
+            </View>
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 10,
+        borderRadius: 25,
+        backgroundColor: "#FFF",
+        shadowColor: "#000",
+        shadowOffset: {width: 0, height: -1},
+        shadowOpacity: 0.05,
+        shadowRadius: 1.0,
+        elevation: 2,
+        marginTop: -50,
+    },
+    closeButtonContainer: {
+        alignItems: "flex-end",
+        padding: 5,
+        marginTop: -20,
+        marginRight: 20,
+    },
+    closeButton: {
+        borderWidth: 0,
+        alignItems: "center",
+        justifyContent: "center",
+        width: 40,
+        height: 40,
+        backgroundColor: "#fff",
+        borderRadius: 30,
+        borderColor: "#FFF",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    qrContainer: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        paddingTop: "12%",
+    },
+});
