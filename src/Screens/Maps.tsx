8a02f3befebf3ac4cf4eeaa308db04c60b9edd34
timepigeon <anatol@riseup.net>
2020-04-21 12:01:22 +0200

    Add maps placeholder modal header

--- src/Screens/Maps.tsx
+++ src/Screens/Maps.tsx
@@ -1,31 +1,82 @@
-import {createStackNavigator} from "@react-navigation/stack";
+import {AntDesign} from "@expo/vector-icons";
 import React from "react";
-import {Text, TouchableOpacity} from "react-native";
-import {Center} from "../Center";
-import {MapsProps} from "../Types";
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {QRCode} from "react-native-custom-qr-codes-expo";
+import {Header} from "../Header";
 
-const Stack = createStackNavigator<MapsProps>();
-
-const MapsView = ({navigation}) => {
+export const Maps = ({navigation}) => {
+    let content = "smile QR Code test";
     return (
-        <Center>
-            <Text>Maps</Text>
-            <TouchableOpacity onPress={navigation.goBack}>
-                <Text style={{marginTop: 50, color: "blue"}}>Zurück</Text>
-            </TouchableOpacity>
-        </Center>
+        <View style={{flex: 1}}>
+            <Header
+                text="Tourvorbereitung"
+                color="#729628"
+                containerStyle={{
+                    flex: 2,
+                    paddingLeft: "10%",
+                    paddingTop: "10%",
+                    alignItems: "flex-start",
+                    paddingBottom: "3%",
+                    backgroundColor: "#F2F2F2",
+                }}
+            />
+            <View style={styles.container}>
+                <View style={styles.closeButtonContainer}>
+                    <TouchableOpacity
+                        onPress={() => navigation.goBack()}
+                        style={styles.closeButton}
+                    >
+                        <AntDesign name={"close"} size={20} color="#f89e3b" />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.mapsContainer}>
+                    <Text>[Maps]</Text>
+                </View>
+            </View>
+        </View>
     );
 };
 
-export const Maps: React.FC<{}> = ({}) => {
-    return (
-        <Stack.Navigator
-            initialRouteName="Maps"
-            screenOptions={{
-                header: () => null,
-            }}
-        >
-            <Stack.Screen options={{title: "Karte"}} name="Maps" component={MapsView} />
-        </Stack.Navigator>
-    );
-};
+const styles = StyleSheet.create({
+    container: {
+        flex: 10,
+        borderRadius: 25,
+        backgroundColor: "#FFF",
+        shadowColor: "#000",
+        shadowOffset: {width: 0, height: -1},
+        shadowOpacity: 0.05,
+        shadowRadius: 1.0,
+        elevation: 2,
+        marginTop: -50,
+    },
+    closeButtonContainer: {
+        alignItems: "flex-end",
+        padding: 5,
+        marginTop: -20,
+        marginRight: 20,
+    },
+    closeButton: {
+        borderWidth: 0,
+        alignItems: "center",
+        justifyContent: "center",
+        width: 40,
+        height: 40,
+        backgroundColor: "#fff",
+        borderRadius: 30,
+        borderColor: "#FFF",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    mapsContainer: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+    },
+});
