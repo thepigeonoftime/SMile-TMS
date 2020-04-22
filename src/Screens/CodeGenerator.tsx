462eeff10771be196cccecfb51c885212481c908
timepigeon <anatol@riseup.net>
2020-04-22 10:51:56 +0200

    Replace QR Code Generator pkg

--- src/Screens/CodeGenerator.tsx
+++ src/Screens/CodeGenerator.tsx
@@ -1,33 +1,12 @@
-import {createStackNavigator} from "@react-navigation/stack";
 import React from "react";
-import {QRCode} from "react-native-custom-qr-codes-expo";
+import {QRCode} from "react-native-custom-qr-codes";
 import {Center} from "../Center";
-import {GeneratorProps} from "../Types";
 
-const Stack = createStackNavigator<GeneratorProps>();
-
-const GeneratorView = ({navigation}) => {
+export const CodeGenerator = ({navigation}) => {
     let content = "smile QR Code test";
     return (
         <Center>
             <QRCode codeStyle="square" content={content} size={300} />
         </Center>
-    );
-};
-
-export const CodeGenerator: React.FC<{}> = ({}) => {
-    return (
-        <Stack.Navigator
-            initialRouteName="CodeGenerator"
-            screenOptions={{
-                header: () => null,
-            }}
-        >
-            <Stack.Screen
-                options={{title: "QR Generator"}}
-                name="CodeGenerator"
-                component={GeneratorView}
-            />
-        </Stack.Navigator>
     );
 };
