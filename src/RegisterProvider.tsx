caab627d8fa1fbaf941dd17830e478e3a105d8e3
timepigeon <anatol@riseup.net>
2020-04-27 15:10:56 +0200

    Post request promise for register data

--- src/RegisterProvider.tsx
+++ src/RegisterProvider.tsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import {AsyncStorage} from "react-native";
+import Axios from "axios";
 
 type registerType = {} | null;
 
@@ -10,7 +11,7 @@ export const RegisterContext = React.createContext<{
     dataGebiet: any;
     dataZeiten: any;
     showRegModal: boolean;
-    register: () => void;
+    register: () => any;
     unregister: () => void;
     storeDataPerson: (data) => void;
     storeDataFahrzeug: (data) => void;
@@ -52,17 +53,42 @@ export const RegisterProvider: React.FC<RegisterProviderProps> = ({children}) =>
                 dataZeiten,
                 showRegModal,
                 register: () => {
-                    setRegistered("smile");
-                    AsyncStorage.setItem("registered", "true");
-                    console.log("registered");
+                    return new Promise((resolve, reject) => {
+                        Axios.post("http://127.0.0.1:3001/registerData", dataPerson)
+                            .then((response) => {
+                                console.log(response.status);
+                                setRegistered("smile");
+                                AsyncStorage.setItem("registered", "true");
+                                console.log("registered");
+                                resolve("registered");
+                            })
+                            .catch((error) => {
+                                if (!error.status) {
+                                    // network error
+                                    console.log("network error");
+                                } else if (error.request) {
+                                    // no response
+                                    console.log(error.request);
+                                } else if (error.response) {
+                                    // non 200
+                                    console.log(error.response.data);
+                                    console.log(error.response.status);
+                                    console.log(error.response.headers);
+                                } else {
+                                    // request error
+                                    console.log("Error", error.message);
+                                }
+                                reject("reg error");
+                            });
+                    });
                 },
                 unregister: () => {
                     setRegistered(null);
-                    AsyncStorage.removeItem("registered");
-                    AsyncStorage.removeItem("dataPerson");
-                    AsyncStorage.removeItem("dataFahrzeug");
-                    AsyncStorage.removeItem("dataGebiet");
-                    AsyncStorage.removeItem("dataZeiten");
+                    // AsyncStorage.removeItem("registered");
+                    // AsyncStorage.removeItem("dataPerson");
+                    // AsyncStorage.removeItem("dataFahrzeug");
+                    // AsyncStorage.removeItem("dataGebiet");
+                    // AsyncStorage.removeItem("dataZeiten");
                     console.log("unregistered");
                 },
                 storeDataPerson: (data) => {
