82e4c82af0ddb8bc4407c4efa6477cf1617bcae8
timepigeon <anatol@riseup.net>
2020-06-05 14:31:18 +0200

    Optimize PaketeScannen scan logic

--- src/Screens/PaketeScannen.tsx
+++ src/Screens/PaketeScannen.tsx
@@ -6,36 +6,57 @@ import {Button} from "react-native-elements";
 import {Header} from "../Header";
 import {TourContext} from "../TourProvider";
 
-const {width} = Dimensions.get("window");
-const qrSize = width * 0.7;
-
 export const PaketeScannen = ({navigation}) => {
     const {tour, reportPickup, setStop} = useContext(TourContext);
     const [hasPermission, setHasPermission] = useState(null);
-    const [scanBackground, setScanBackground] = useState("#FFF");
-    const [type, setType] = useState(Camera.Constants.Type.back);
     const [finished, setFinished] = useState(false);
-    const packets = [];
+    const [scannedPackets, setScannedPackets] = useState(0);
+    const [scanMsg, setScanMsg] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [packets, setPackets] = useState([]);
+    const [packetLen, setPacketLen] = useState(0);
+
+    const tourPackets = tour.packets.map((packet) => {
+        return packet.sscc;
+    });
 
     useEffect(() => {
         (async () => {
             const {status} = await BarCodeScanner.requestPermissionsAsync();
             setHasPermission(status === "granted");
         })();
-        tour.packets.map((packet) => {
-            packets.push(packet.sscc);
-        });
+
+        setPackets(tourPackets);
+        setPacketLen(tourPackets.length);
+        setLoading(false);
     }, []);
+
+    const showScanMsg = (msg) => {
+        setScanMsg(msg);
+        setTimeout(() => {
+            setScanMsg("");
+        }, 5000);
+    };
 
     const handleBarCodeScanned = ({type, data}) => {
         const sscc = "urn:epc:id:sscc:" + data;
-        packets.map((packet) => {
-            console.log("packet:", packet);
-            console.log("data:", sscc);
-            if ("urn:epc:id:sscc:" + data === packet) {
-                packets.splice(packets.indexOf(packet, 1));
+        console.log(packets);
+        let packetBuffer = [];
+        packets.forEach((packet) => {
+            if (sscc === packet) {
+                setScannedPackets(scannedPackets + 1);
+                reportPickup(packet.sscc, new Date().toJSON());
+            } else {
+                packetBuffer.push(packet);
             }
         });
+        setPackets(packetBuffer);
+        if (!tourPackets.includes(sscc)) {
+            showScanMsg("Unbekanntes Paket oder Code!");
+        }
+        // else {
+        //     showScanMsg("Paket bereits gescannt!");
+        // }
         if (!packets.length) {
             setFinished(true);
         }
@@ -69,27 +90,36 @@ export const PaketeScannen = ({navigation}) => {
                 }}
             />
             <View style={styles.container}>
+                <View style={{alignItems: "center"}}>
+                    <Text>{scanMsg}</Text>
+                </View>
                 <View style={styles.scannerContainer}>
-                    <Camera
-                        style={styles.scanner}
-                        type={type}
-                        onBarCodeScanned={handleBarCodeScanned}
-                        autoFocus={"on"}
-                        ratio="4:3"
-                    />
-
-                    <View style={styles.buttonContainer}>
-                        {finished && (
-                            <Button
-                                buttonStyle={styles.button}
-                                title="Tour Starten"
-                                onPress={onSubmit}
-                            />
-                        )}
-                        <TouchableOpacity onPress={onSubmit}>
-                            <Text>skip</Text>
-                        </TouchableOpacity>
-                    </View>
+                    {!loading && (
+                        <Camera
+                            style={styles.scanner}
+                            type={Camera.Constants.Type.back}
+                            onBarCodeScanned={handleBarCodeScanned}
+                            autoFocus={"on"}
+                            ratio="16:9"
+                        />
+                    )}
+                </View>
+                <View style={{justifyContent: "center", alignItems: "center"}}>
+                    <Text>
+                        {scannedPackets} von {packetLen} Paketen gescannt
+                    </Text>
+                </View>
+                <View style={styles.buttonContainer}>
+                    {finished && (
+                        <Button
+                            buttonStyle={styles.button}
+                            title="Tour Starten"
+                            onPress={onSubmit}
+                        />
+                    )}
+                    <TouchableOpacity onPress={onSubmit}>
+                        <Text>skip</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         </View>
@@ -107,29 +137,35 @@ const styles = StyleSheet.create({
         shadowRadius: 1.0,
         elevation: 2,
         justifyContent: "center",
+        alignItems: "center",
     },
     scannerContainer: {
-        flex: 1,
+        // flex: 1,
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        overflow: "hidden",
-        width: "100%",
-        height: "50%",
-        marginTop: "10%",
-    },
-    scanner: {
-        backgroundColor: "#ecf0f1",
-        padding: 8,
         borderRadius: 30,
+        overflow: "hidden",
         width: "90%",
         height: "50%",
+        marginTop: "10%",
+        borderWidth: 5,
+    },
+    scanner: {
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#ecf0f1",
+        padding: 8,
+        // borderRadius: 30,
+        width: "100%",
+        height: "140%",
     },
     buttonContainer: {
         flex: 1,
         alignItems: "center",
         marginTop: "5%",
         width: "100%",
+        height: "140%",
     },
     button: {
         backgroundColor: "#3FA9F5",
