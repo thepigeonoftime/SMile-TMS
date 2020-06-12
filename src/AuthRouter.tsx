12a94ffdb34ccbecd87a7e96deab665c22dc9d3f
timepigeon <anatol@riseup.net>
2020-06-12 14:55:01 +0200

    Show Splash screen on Auth loading

--- src/AuthRouter.tsx
+++ src/AuthRouter.tsx
@@ -1,10 +1,11 @@
 import {NavigationContainer} from "@react-navigation/native";
 import * as SecureStore from "expo-secure-store";
 import React, {useContext, useEffect, useState} from "react";
-import {View} from "react-native";
+import {View, ImageBackground, ActivityIndicator} from "react-native";
 import {AppContainer} from "./AppContainer";
 import {AuthContext} from "./AuthProvider";
 import {AuthStack} from "./AuthStack";
+import {Center} from "./Center";
 
 export const AuthRouter: React.FC<{}> = ({}) => {
     const {token, storeToken} = useContext(AuthContext);
@@ -24,7 +25,13 @@ export const AuthRouter: React.FC<{}> = ({}) => {
     }, []);
 
     if (loading) {
-        return <View>{/* Splash Screen */}</View>;
+        return (
+            <ImageBackground source={require("../assets/splash.png")} style={{flex: 1}}>
+                <Center>
+                    <ActivityIndicator size="small" color="#FFF" />
+                </Center>
+            </ImageBackground>
+        );
     }
     // route to AppContainer if JWT token is set, otherwise show AuthStack
     return <NavigationContainer>{token ? <AppContainer /> : <AuthStack />}</NavigationContainer>;
